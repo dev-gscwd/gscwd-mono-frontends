a8d2deb499ba796734db0502098bd7b06df474d0
ericsison <ericsison.dev@gmail.com>
2022-11-21 09:48:01 +0800

    removed component test pages

--- apps/ims/src/pages/sidebar.tsx
+++
@@ -1,7 +0,0 @@
-export default function SidebarTest() {
-  return (
-    <div className="h-screen w-screen">
-      <div>Content</div>
-    </div>
-  );
-}
--- apps/ims/src/pages/testings.tsx
+++
@@ -1,3 +0,0 @@
-export default function Testings() {
-  return <div className="flex h-full justify-center items-center">tessst</div>;
-}
