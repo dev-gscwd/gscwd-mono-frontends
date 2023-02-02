2140c07dde9762eeef5172c640aadfe55d01fc98
jayrln <jaynosotros@gmail.com>
2023-02-02 13:26:58 +0700

    added item details page

--- apps/ims/src/pages/fe/items/[item_id].tsx
+++ apps/ims/src/pages/fe/items/[item_id].tsx
@@ -1,13 +1,40 @@
 import { Button, TextField } from '@frontends/shared-ui';
 import { url } from 'inspector';
 import { useState } from 'react';
-import { HiEllipsisVertical } from 'react-icons/hi2';
+import {
+  HiEllipsisVertical,
+  HiOutlineMagnifyingGlass,
+  HiOutlineChevronRight,
+  HiOutlineChevronLeft,
+} from 'react-icons/hi2';
+import { Tab } from '@headlessui/react';
 
 // const [classification, setClassification] = useState();
 
 export default function ItemDetails() {
   return (
     <div className="w-full h-full">
+      {/* <div className="relative w-full"> */}
+      <div className="flex justify-between">
+        <div></div>
+        {/* <div className="flex flex-row items-center">
+          <HiOutlineChevronLeft size={28} />
+          <HiOutlineChevronRight size={28} />
+        </div> */}
+        <div>
+          {/* <Button> */}
+          {/* <TextField placeholder="Search Items" size={40} className="rounded-2xl" /> */}
+
+          {/* </Button> */}
+          {/* <HiOutlineChevronLeft />
+          <HiOutlineChevronRight /> */}
+        </div>
+        {/* <TextField placeholder="Search" className="mr-0" /> */}
+      </div>
+      {/* <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+          <HiOutlineMagnifyingGlass />
+        </div> */}
+      {/* </div> */}
       <div className="bg-white p-5 rounded-md">
         <div className="flex justify-between">
           <div className="flex">
@@ -19,88 +46,76 @@ export default function ItemDetails() {
               />
             </div>
             <div className="ml-6">
-              <div>
-                <p className="text-2xl">Hammer, Ball Hammer Heavy Duty</p>
-              </div>
-              <div>04-277-001</div>
-              <div className="pt-10">
+              <div className="flex flex-col h-full justify-between">
+                <div>
+                  <p className="text-2xl">Hammer, Ball Hammer Heavy Duty</p>
+                  <div>04-277-001</div>
+                </div>
                 <div>
                   <p>Available Quantity: 50</p>
+                  <div>Average Cost: 290.00</div>
                 </div>
-                <div>Average Cost: 290.00</div>
               </div>
             </div>
           </div>
           <div>
-            <div className="flex flex-col items-end">
+            <div className="flex flex-col items-end h-full justify-between">
               <div>
-                <HiEllipsisVertical size={28} />
+                <HiEllipsisVertical size={28} className="hover:fill-gray-500" />
               </div>
-              <div className="mt-14">
+              <div>
                 <p className="text-sm italic">Date Created: 09/25/2014</p>
               </div>
             </div>
           </div>
         </div>
-        {/* <hr className="my-4" /> */}
-        <div className="mb-4 border-b border-gray-200 dark:border-gray -700">
-          <ul
-            className="flex flex-wrap -mb-px text-sm font-medium text-center"
-            id="myTab"
-            data-tabs-toggle="#myTabContent"
-            role="tablist"
-          >
-            <li className="mr-2" role="presentation">
-              <button
-                className="inline-block p-4 rounded-t-lg border-b-2"
-                id="details-tab"
-                data-tabs-target="#details"
-                type="button"
-                role="tab"
-                aria-controls="details"
-                aria-selected="false"
-              >
-                Details
-              </button>
-            </li>
-            <li className="mr-2" role="presentation">
-              <button
-                className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="dashboard-tab"
-                data-tabs-target="#dashboard"
-                type="button"
-                role="tab"
-                aria-controls="dashboard"
-                aria-selected="false"
-              >
-                Bin Card
-                {/* (Warehouse) */}
-              </button>
-            </li>
-            <li className="mr-2" role="presentation">
-              <button
-                className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="dashboard-tab"
-                data-tabs-target="#dashboard"
-                type="button"
-                role="tab"
-                aria-controls="dashboard"
-                aria-selected="false"
-              >
-                Stock Card
-                {/* (Accounting) */}
-              </button>
-            </li>
-          </ul>
+        <div className="mt-4">
+          <Tab.Group>
+            <Tab.List className={'mb-4 border-b border-gray-200'}>
+              <Tab className={'pr-2 py-4 hover:text-gray-500'}>Details</Tab>
+              <Tab className={'px-2 py-4 hover:text-gray-500'}>Bin Card</Tab>
+              <Tab className={'px-2 py-4 hover:text-gray-500'}>Stock Card</Tab>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
+                <div className="flex">
+                  <div className="">Characteristics:</div>
+                  <div>Physical</div>
+                </div>
+                <div className="flex">
+                  <div className="">Classification:</div>
+                  <div>Expense</div>
+                </div>
+                <div className="flex">
+                  <div className="">Category:</div>
+                  <div>Gutter</div>
+                </div>
+                <div className="flex">
+                  <div className="">Specification:</div>
+                  <div>0.400mmx0.915x2.4m</div>
+                </div>
+                <div className="flex">
+                  <div className="">UOM:</div>
+                  <div>PC</div>
+                </div>
+                <div className="flex">
+                  <div className="">Reorder Point:</div>
+                  <div>0</div>
+                </div>
+                <div className="flex">
+                  <div className="">Reorder Quantity:</div>
+                  <div>0</div>
+                </div>
+                <div className="flex">
+                  <div className="">On Order:</div>
+                  <div>0</div>
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>Content 2</Tab.Panel>
+              <Tab.Panel>Content 3</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
         </div>
-        {/* <div>
-          <div className="grid grid-cols-2 gap-6">
-            <div className="">
-              <label>Classification</label>
-              <TextField className="w-full pointer-events-none" defaultValue={'Expense'} disabled />
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
